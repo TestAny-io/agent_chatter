a1914b587f2e6120d9ac855ea5a4ba3105aee713
kl-testany <kl@testany.io>
2025-11-29 23:36:44 +0800

    chore: remove CLI package and scope repo to core only

--- packages/cli/src/config/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * CLI Config Module Exports
- *
- * @file src/cli/config/index.ts
- * @layer CLI
- */
-
-export { type UIPreferences, DEFAULT_UI_PREFERENCES } from './UIPreferences.js';
-export { ConfigAdapter, splitConfig, mergeConfig } from './ConfigAdapter.js';
